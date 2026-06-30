ab96eeda01c75f95bb4b315d6e6f37c65e85d66f
ntorracca <ntorracca@gmail.com>
2026-06-30 15:27:23 -0300

    feat: :sparkles: add: parte2-aula02 adicionando cors

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const rotaLivro = require('./rotas/livro')
 const app = express()
+const cors = require("cors")
 app.use(express.json())
-
+app.use(cors({origin: "*"}))
 const port = 8000
 
 app.use("/livros", rotaLivro)
